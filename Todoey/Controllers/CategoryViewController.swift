b9ec3fa706bc856ec5a231c4e1add3d19320ce51
waiga <hyang9387@gmail.com>
2022-02-14 23:48:03 +0900

    CategoryViewController Finish

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,97 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by 김미향 on 2022/02/14.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+//    let categoryArray = [Category]()
+    var categoryArray = [Category]()
+    
+    //read, create, load, delete, update
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadCategory()
+        
+
+
+    }
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        //let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = categoryArray[indexPath.row].name
+        
+        return cell
+    }
+
+    //MARK: - Data Manipulation Methods
+    func saveCategory() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error Saving Context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadCategory(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error Fetching Data from context \(error)")
+        }
+
+        tableView.reloadData()
+    }
+    
+    //MARK: - Add new Categories
+    
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: ":->", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ADD CATEGORY", style: .default) { action in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.categoryArray.append(newCategory)
+            self.saveCategory()
+        }
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create New Category:^o"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+    
+}
