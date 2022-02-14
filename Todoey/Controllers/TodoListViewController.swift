1b67a2b4fafb1f9be2598b07f1d596a64a8b1282
waiga <hyang9387@gmail.com>
2022-02-14 12:47:01 +0900

    Instead {encode copy and paste }, making func

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,15 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
+//    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
-
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        print(dataFilePath)
         
         let newItem = Item()
         newItem.title = "Find Milk"
-        newItem.done = true
         itemArray.append(newItem)
         
         let newItem2 = Item()
@@ -29,9 +30,8 @@ class TodoListViewController: UITableViewController {
         let newItem3 = Item()
         newItem3.title = "item3"
         itemArray.append(newItem3)
-        
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+                
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //            itemArray = items
 //        }
         
@@ -45,16 +45,16 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        print("Cellfor Row At IndexPath Call")
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        let item = itemArray[indexPath.row]
         
-        if itemArray[indexPath.row].done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
+        cell.textLabel?.text = item.title
+        
+        // Ternary Operator ->
+        // value = condition ? value if true : value if false
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -64,8 +64,7 @@ class TodoListViewController: UITableViewController {
         // print(itemArray[indexPath.row])
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        
+
         tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -75,20 +74,25 @@ class TodoListViewController: UITableViewController {
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
             //what will happen once the user clicks the Add Item button on our UIAlert
-            
             
             let newItem = Item()
             newItem.title = textField.text!
             
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            //self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            let encoder = PropertyListEncoder()
             
+            do {
+                let data = try encoder.encode(self.itemArray)
+                try data.write(to: self.dataFilePath!)
+            } catch {
+                print("Error encoding item array, \(error)")
+                
+            }
             self.tableView.reloadData()
         }
         
